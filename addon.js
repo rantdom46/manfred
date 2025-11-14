78aeb1c0c07cf213efc1101f346015c211555433
Antho <rantdomgaming@gmail.com>
2025-11-14 10:47:54 +0100

    added a new js file

---
+++ addon.js
@@ -0,0 +1,32 @@
+const listItems = document.querySelectorAll("ul li");
+const beerImages = document.querySelectorAll(".beer-images img");
+
+function resetActive() {
+  beerImages.forEach(img => img.classList.remove("active"));
+  listItems.forEach(li => li.classList.remove("active"));
+}
+
+listItems.forEach(item => {
+  const beerId = item.dataset.beer;
+  const targetImg = document.getElementById(beerId);
+
+  // Hover (desktop)
+  item.addEventListener("mouseenter", () => {
+    resetActive();
+    targetImg.classList.add("active");
+    item.classList.add("active");
+  });
+  item.addEventListener("mouseleave", () => { resetActive(); });
+
+  // Click / tap (mobile)
+  item.addEventListener("click", () => {
+    if(targetImg.classList.contains("active")) {
+      targetImg.classList.remove("active");
+      item.classList.remove("active");
+    } else {
+      resetActive();
+      targetImg.classList.add("active");
+      item.classList.add("active");
+    }
+  });
+});
